0c7ff716b6e2ea5556126d847f3ac7cb373ce77d
Peter Jang <peter.x.jang@gmail.com>
2018-07-31 18:57:24 -0500

    Use v-for to display array of objects example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,12 @@
     <h1>{{ message }}</h1>
     <input type="text" v-model="message">
     <h2>{{ message2 }}</h2>
+    <h1>Reviews</h1>
+    <ul>
+      <li v-for="review in reviews">
+        {{ review.rating }}: {{ review.text }} - by {{ review.reviewer }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -14,7 +20,12 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!!!",
-      message2: "yo"
+      message2: "yo",
+      reviews: [
+        { text: "Vue.js is cool", rating: 5, reviewer: "Burton Wax" },
+        { text: "Vue.js is great", rating: 4, reviewer: "Emily Post" },
+        { text: "I like checkers", rating: 1, reviewer: "Angela Pearson" }
+      ]
     };
   },
   created: function() {},
