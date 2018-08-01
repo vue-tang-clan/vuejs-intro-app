199e11acd6e91aacc63b2c8656ec618ef80cd7bf
Peter Jang <peter.x.jang@gmail.com>
2018-07-31 19:43:01 -0500

    Use v-on:click to add a new review example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,12 @@
     <h1>{{ message }}</h1>
     <input type="text" v-model="message">
     <h2>{{ message2 }}</h2>
+    <h1>Add a review</h1>
+    Review text: <input type="text" v-model="newReview.text">
+    Review rating: <input type="text" v-model="newReview.rating">
+    Review name: <input type="text" v-model="newReview.reviewer">
+    <button v-on:click="addReview()">Add review</button>
+
     <h1>Reviews</h1>
     <ul>
       <li v-for="review in reviews">
@@ -25,11 +31,17 @@ export default {
         { text: "Vue.js is cool", rating: 5, reviewer: "Burton Wax" },
         { text: "Vue.js is great", rating: 4, reviewer: "Emily Post" },
         { text: "I like checkers", rating: 1, reviewer: "Angela Pearson" }
-      ]
+      ],
+      newReview: { text: "", rating: "", reviewer: "" }
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    addReview: function() {
+      console.log("nice...", this.newReview);
+      this.reviews.push(this.newReview);
+    }
+  },
   computed: {}
 };
 </script>
